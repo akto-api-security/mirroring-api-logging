229bd9f4e8e33c4e019cba890702adce392007a4
Avneesh Hota <avneesh@akto.io>
2023-04-18 13:53:05 +0530

    traffic of all ports except 22

--- tcpdump.sh
+++ tcpdump.sh
@@ -2,5 +2,5 @@
 while :
 do
     printf "running tcpdump\n"
-    tcpdump -i eth0 port 8080 -w /app/files/%s  -W 120 -G 30 -K -n
+    tcpdump -i eth0 port not 22 -w /app/files/%s  -W 120 -G 30 -K -n
 done
