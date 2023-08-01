1cd9bc9af24a0dfa52a68f978977cbb3e24e1d3c
ayushaga14 <ayush@akto.io>
2023-08-01 12:15:31 +0530

    fix

--- main.go
+++ main.go
@@ -670,7 +670,7 @@ func logKafkaStats() {
 		"BatchTimeout %v, ReadTimeout %v, WriteTimeout %v, RequiredAcks %d, Async %t, Topic %s", stats.Dials,
 		stats.Writes, stats.Messages, stats.Bytes, stats.Errors, stats.DialTime, stats.BatchTime, stats.WriteTime,
 		stats.WaitTime, stats.Retries, stats.BatchSize, stats.BatchBytes, stats.MaxAttempts, stats.MaxBatchSize,
-		stats.BatchTimeout, stats.ReadTimeout, stats.WriteTimeout, stats.RequiredAcks, stats.Async, stats.Topic)git
+		stats.BatchTimeout, stats.ReadTimeout, stats.WriteTimeout, stats.RequiredAcks, stats.Async, stats.Topic)
 	//log.Println(kafkaWriter.Stats())
 }
 
