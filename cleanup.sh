ef17bebeb5c8ff6eb34e6b8a63c3180e3c91f37e
Avneesh Hota <avneesh@akto.io>
2024-10-15 15:33:21 +0530

    modified cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
 max=80
-DIR="/app/files"
+base_dir="/app"  # Base directory for all files
 
 while true
 do
-  printf "running cleanup\n"
-  available=$(df -P /app | awk '{ gsub("%",""); capacity = $5 }; END { print capacity }')
+  printf "Running cleanup\n"
 
+  # Get the current disk usage percentage for /app
+  available=$(df -P "$base_dir" | awk '{ gsub("%",""); capacity = $5 }; END { print capacity }')
+
+  # If the available disk usage is greater than the max (80%)
   if [ "$available" -gt "$max" ]; then
     printf "Available value greater than max\n"
-    find "$DIR" -type f -delete
+
+    # Find and delete all files in directories /app/files_*
+    find "$base_dir" -type f -name "*.pcap" -delete
   fi
 
-  find "$DIR" -type f -mmin +2 -delete
+  # Delete files older than 2 minutes in directories /app/files_*
+  find "$base_dir" -type f -name "*.pcap" -mmin +2 -delete
+
+  # Sleep for 30 seconds before running the next cleanup cycle
   sleep 30
 done
-
