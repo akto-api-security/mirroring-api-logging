af1ac7d88d161d203489723bbfe7175a300b69af
ayushaga14 <ayush@akto.io>
2025-03-03 13:07:29 +0530

    remove gc logs

--- trafficUtil/utils/stats.go
+++ trafficUtil/utils/stats.go
@@ -28,13 +28,13 @@ func LogMemoryStats() int {
 
 	// gc stats
 
-	log.Println("Last gc finished: ", m.LastGC)
-	log.Println("Target heap size of next gc cycle: ", m.NextGC)
-	log.Println("Stop The world pauses ", m.PauseTotalNs)
-	log.Println("GcSys ", m.GCSys)
-	log.Println("GCCPUFraction ", m.GCCPUFraction)
-	log.Println("NumGC ", m.NumGC)
-	log.Println("NumForcedGC ", m.NumForcedGC)
+	//log.Println("Last gc finished: ", m.LastGC)
+	//log.Println("Target heap size of next gc cycle: ", m.NextGC)
+	//log.Println("Stop The world pauses ", m.PauseTotalNs)
+	//log.Println("GcSys ", m.GCSys)
+	//log.Println("GCCPUFraction ", m.GCCPUFraction)
+	//log.Println("NumGC ", m.NumGC)
+	//log.Println("NumForcedGC ", m.NumForcedGC)
 
 	return mem
 }
