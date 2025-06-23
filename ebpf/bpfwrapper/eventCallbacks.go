f8f8bb268db576e73571331b946ec2f3b9615095
ayushaga14 <ayush@akto.io>
2025-06-24 00:06:15 +0530

    comment out SocketDataEventCallback

--- ebpf/bpfwrapper/eventCallbacks.go
+++ ebpf/bpfwrapper/eventCallbacks.go
@@ -95,6 +95,10 @@ func min(a, b int32) int32 {
 
 func SocketDataEventCallback(data []byte, connectionFactory *connections.Factory) {
 	// for data := range inputChan {
+	if true {
+		return
+	}
+
 	if data == nil {
 		return
 	}
