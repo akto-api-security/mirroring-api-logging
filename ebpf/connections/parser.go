3da8751d369e663d9fb5d9ee42ddf9776f09ad7e
Ankush Jain <ankush@akto.io>
2026-05-07 21:59:13 -0700

    skip kafka

--- ebpf/connections/parser.go
+++ ebpf/connections/parser.go
@@ -1,8 +1,19 @@
 package connections
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/akto-api-security/mirroring-api-logging/trafficUtil/kafkaUtil"
 )
+
+var printToStdout bool
+
+func init() {
+	printToStdoutEnv := os.Getenv("PRINT_TRAFFIC_TO_STDOUT")
+	printToStdout = len(printToStdoutEnv) > 0 && strings.EqualFold(printToStdoutEnv, "true")
+}
 
 func tryReadFromBD(ip string, destIp string, receiveBuffer []byte, sentBuffer []byte, isComplete bool, direction int, id uint64, fd uint32, daemonsetIdentifier, hostName string) {
 	ctx := kafkaUtil.TrafficContext{
@@ -18,5 +29,17 @@ func tryReadFromBD(ip string, destIp string, receiveBuffer []byte, sentBuffer []
 		DaemonsetIdentifier: daemonsetIdentifier,
 		HostName:            hostName,
 	}
+
+	if printToStdout {
+		// Print traffic summary to stdout instead of Kafka
+		directionStr := "ingress"
+		if direction == 2 {
+			directionStr = "egress"
+		}
+		fmt.Printf("[TRAFFIC] direction=%s src=%s dst=%s processId=%d fd=%d daemon=%s host=%s complete=%v recvBytes=%d sentBytes=%d\n",
+			directionStr, ip, destIp, uint32(id>>32), fd, daemonsetIdentifier, hostName, isComplete, len(receiveBuffer), len(sentBuffer))
+		return
+	}
+
 	kafkaUtil.ParseAndProduce(receiveBuffer, sentBuffer, ctx)
 }
