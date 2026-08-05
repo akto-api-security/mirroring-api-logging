98d63838efe89b92aa3c917b8c9ce9ee2e8388da
gauravmann <gaurav@akto.io>
2026-08-05 09:24:11 +0530

    remove comment from pragma unroll

--- ebpf/kernel/module.cc
+++ ebpf/kernel/module.cc
@@ -480,7 +480,7 @@ static __inline void process_syscall_data(struct pt_regs* ret, const struct data
     int bytes_sent = 0;
     size_t size_to_save = 0;
     int i =0;
-  // #pragma unroll
+  #pragma unroll
   for (i = 0; i < CHUNK_LIMIT; ++i) {
     const int bytes_remaining = bytes_exchanged - bytes_sent;
 
