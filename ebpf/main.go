f7c32100e80ff38fd60580101b50bb8652135f45
gauravmann <gaurav@akto.io>
2026-08-10 14:12:46 +0530

    re-add config consumer

--- ebpf/main.go
+++ ebpf/main.go
@@ -143,7 +143,7 @@ func run() {
 	apiProcessor.InitCloudTrafficProcessor()
 	kafkaUtil.InitKafka()
 
-	// kafkaUtil.StartConfigConsumer()
+	kafkaUtil.StartConfigConsumer()
 
 	stopCh, err := kafkaUtil.SetupPodInformer()
 	if err != nil {
