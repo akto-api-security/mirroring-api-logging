e68dac2d6175a254c1fac06071ee6142a3a128f0
gauravmann <gaurav@akto.io>
2026-08-12 13:03:08 +0530

    send heartbeat first.

--- ebpf/main.go
+++ ebpf/main.go
@@ -103,6 +103,7 @@ func run() {
 	}
 	source = string(byteString)
 
+	kafkaUtil.InitKafka()
 	replaceBpfLogsMacros()
 	replaceBpfChunkSizeMacros()
 	replaceMaxConnectionMapSize()
@@ -122,7 +123,6 @@ func run() {
 
 	// this needs to be called before InitKafka
 	apiProcessor.InitCloudTrafficProcessor()
-	kafkaUtil.InitKafka()
 
 	kafkaUtil.StartConfigConsumer()
 
